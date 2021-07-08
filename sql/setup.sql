dd9f44044b7396feab0c0e7b481860ba21bf3d71
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-08 14:29:58 -0400

    commented our references for now

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,39 +2,34 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS finds CASCADE;
 DROP TABLE IF EXISTS spots CASCADE;
 DROP TABLE IF EXISTS photos;
-
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   username TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
   phone_number VARCHAR(15) NOT NULL
 );
-
 CREATE TABLE finds (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id REFERENCES users(id),
+  -- user_id REFERENCES users(id),
   title TEXT NOT NULL,
   is_claimed BOOLEAN NOT NULL,
-  latitude DECIMAL(8,6) NOT NULL,
-  longitude DECIMAL(9,6) NOT NULL,
+  latitude DECIMAL(8, 6) NOT NULL,
+  longitude DECIMAL(9, 6) NOT NULL,
   category TEXT NOT NULL,
-  tags TEXT[],
-  created_at TIMESTAMP DEFAULT NOW() 
+  tags TEXT [],
+  created_at TIMESTAMP DEFAULT NOW()
 );
-
 CREATE TABLE spots (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id REFERENCES users(id),
+  -- user_id INT users(id),
   name TEXT NOT NULL,
   radius INT NOT NULL,
-  latitude DECIMAL(8,6) NOT NULL,
-  longitude DECIMAL(9,6) NOT NULL
-  items TEXT[] 
+  latitude DECIMAL(8, 6) NOT NULL,
+  longitude DECIMAL(9, 6) NOT NULL
 );
-
 CREATE TABLE photos (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id REFERENCES users(id),
-  find_id REFERENCES finds(id),
+  -- user_id REFERENCES users(id),
+  -- find_id REFERENCES finds(id),
   photo TEXT NOT NULL
 );
