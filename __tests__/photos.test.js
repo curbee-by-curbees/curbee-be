79ac9b4ce718fb5c5824ddb0a0fb1f27cc74b8e1
Clem Hepburn <clemhepburn@gmail.com>
2021-07-08 15:06:18 -0700

    create failing photos POST test

--- __tests__/photos.test.js
+++ __tests__/photos.test.js
@@ -2,17 +2,55 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request, { agent } from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
+import { jest } from '@jest/globals'
 
 describe('photos routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+
+
+  let agent;
+  let user, find;
+
+  beforeEach( async () => {
+    setup(pool);
+    agent = await request.agent(app);
+    user = await UserService.create({
+      username: 'me',
+      password: 'password',
+      phoneNumber: '14206661234'
+    });
+    await agent.post('/api/v1/auth/login')
+      .send({
+        username: 'me',
+        password: 'password'
+      });
+    find = await agent
+      .post('/api/v1/finds')
+      .send({
+        title: 'huge dog',
+        isClaimed: false,
+        latitude: '45.519960',
+        longitude: '-122.637980',
+        category: 'scary',
+        tags: ['dog', 'scary', 'hercules']
+      })
+    
   });
 
   it('POST a photo', async () => {
     const res = await agent
       .post('/api/v1/photos')
       .send({
-        
-      })
-  })
+        userId: user.id,
+        findId: find.id,
+        photo: 'nightmare.jpg'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      userId: user.id,
+      findId: find.id,
+      photo: 'nightmare.jpg'
+    });
+  });
 }); 
