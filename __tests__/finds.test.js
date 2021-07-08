db98270bee1001a7b8205800c49ec1a96f67a4e2
cmmerritt <christiane.merritt@gmail.com>
2021-07-08 13:36:36 -0700

    add retrieve all finds test

--- __tests__/finds.test.js
+++ __tests__/finds.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Find from '../lib/models/Find.js';
 
 describe('finds routes', () => {
   beforeEach(() => {
@@ -18,6 +19,16 @@ describe('finds routes', () => {
     tags: ['statue', 'cat']
   };
 
+  let find2 = {
+    // userId: '1',
+    title: 'Marble dog statue',
+    isClaimed: false,
+    latitude: '45.519960',
+    longitude: '-122.637980',
+    category: 'decor',
+    tags: ['statue', 'dog']
+  };
+
   it('creates a find via POST', async () => {
     const res = await request(app)
       .post('/api/v1/finds')
@@ -25,4 +36,15 @@ describe('finds routes', () => {
 
     expect(res.body).toEqual({ ...find1, id: '1', createdAt: expect.any(String) });
   });
+
+  it('retrieves all finds', async () => {
+    const cat = await Find.insert(find1);
+    const dog = await Find.insert(find2);
+
+    const res = await request(app)
+      .get('/api/v1/finds');
+    
+    expect(res.body).toEqual([cat, dog]);
+  });
+
 });
