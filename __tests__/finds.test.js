3226d3b6071f43113a61cf8468d75621b408c544
Katherine Tam <katherinemtam@gmail.com>
2021-07-08 15:25:54 -0700

    add agent, finds put route test

--- __tests__/finds.test.js
+++ __tests__/finds.test.js
@@ -3,14 +3,34 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Find from '../lib/models/Find.js';
+import UserService from '../lib/services/UserService.js';
 
-describe('finds routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+
+describe.only('finds routes', () => {
+  
+  const agent = request.agent(app);
+  let user;
+
+  beforeEach(async() => {
+    await setup(pool);
+
+    user = await UserService.create({
+      username: 'me',
+      password: 'password',
+      phoneNumber: '14206661234' 
+    });
+    
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'me',
+        password: 'password' 
+      });
+
+    return user;
   });
 
-  let find1 = {
-    // userId: '1',
+  const find1 = {
     title: 'Porcelain cat statue',
     isClaimed: false,
     latitude: '45.519960',
@@ -19,8 +39,7 @@ describe('finds routes', () => {
     tags: ['statue', 'cat']
   };
 
-  let find2 = {
-    // userId: '1',
+  const find2 = {
     title: 'Marble dog statue',
     isClaimed: false,
     latitude: '45.519960',
@@ -29,8 +48,8 @@ describe('finds routes', () => {
     tags: ['statue', 'dog']
   };
 
-  it('creates a find via POST', async () => {
-    const res = await request(app)
+  it.only('creates a find via POST', async () => {
+    const res = await agent
       .post('/api/v1/finds')
       .send(find1);
 
@@ -41,7 +60,7 @@ describe('finds routes', () => {
     const cat = await Find.insert(find1);
     const dog = await Find.insert(find2);
 
-    const res = await request(app)
+    const res = await agent
       .get('/api/v1/finds');
     
     const catDateFix = { ...cat, createdAt: expect.any(String) };
@@ -53,11 +72,29 @@ describe('finds routes', () => {
   it('gets find by id', async () => {
     const cat = await Find.insert(find1);
 
-    const res = await request(app)
+    const res = await agent
       .get(`/api/v1/finds/${cat.id}`);
     
     const catDateFix = { ...cat, createdAt: expect.any(String) };
 
     expect(res.body).toEqual(catDateFix);
+  });
+
+  it('updates a find via PUT', async() => {
+    const find = await Find.insert(find1);
+
+    find.isClaimed = true;
+    find.tags = ['statue', 'cat', 'porcelain'];
+
+    const res = await agent
+      .put(`/api/v1/${find.id}`)
+      .send(find);
+
+    expect(res.body).toEqual({
+      ...find, 
+      isClaimed: true, 
+      tags: ['statue', 'cat', 'porcelain']
+    });
+    
   });
 });
