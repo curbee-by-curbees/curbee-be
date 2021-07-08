48851aba7960f92d851fa4ffee39c4f958331535
Tis Lais <tistististis@gmail.com>
2021-07-08 11:38:29 -0700

    Add failing spots get all test

--- __tests__/spots.test.js
+++ __tests__/spots.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Spot from '../lib/models/Spot.js';
+import Spot from '../lib/models/Spot.js';
 
 const home = {
   name: 'home',
@@ -12,6 +12,13 @@ const home = {
   longitude: '-122.675000',
 };
 
+const work = {
+  name: 'work',
+  // userId: '1',
+  radius: 1,
+  latitude: '45.505100',
+  longitude: '-122.675000',
+};
 
 describe('spot routes', () => {
   beforeEach(() => {
@@ -28,6 +35,28 @@ describe('spot routes', () => {
       ...home,
   
     });
+  });
+
+  it('gets all spots via GET', async () => {
+    
+    await Spot.create(home);
+    await Spot.create(work);
+
+    const res = await request(app)
+      .get('/api/v1/spots');
+    
+    const expected = [
+      {
+        id: '1',
+        ...home
+      },
+      {
+        id: '2',
+        ...work
+      }
+    ];
+    
+    expect(res.body).toEqual(expected);
   });
 
 
