d442b8612ba09d7cb351b8f7533158901a8fd4ce
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-08 13:49:27 -0400

    failing spot route

--- __tests__/spots.test.js
+++ __tests__/spots.test.js
@@ -2,33 +2,31 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-// import Find from '../lib/models/Find.js';
+// import Spot from '../lib/models/Spot.js';
 
-const couch = {
-  title: 'couch',
+const home = {
+  name: 'home',
   userId: '1',
-  isClaimed: false,
+  radius: 1,
   latitude: 45.5051,
   longitude: -122.6750,
-  category: 'furniture',
-  tags: ['couch', 'orange', 'sofa']
 };
 
 
-describe('finds routes', () => {
+describe('spot routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a find via POST', async () => {
+  it('creates a spot via POST', async () => {
     const res = await request(app)
-      .post('/api/v1/finds')
-      .send(couch);
+      .post('/api/v1/spots')
+      .send(home);
 
     expect(res.body).toEqual({
       id: 1,
-      ...couch,
-      createdAt: expect.any(String)
+      ...home,
+  
     });
   });
 
