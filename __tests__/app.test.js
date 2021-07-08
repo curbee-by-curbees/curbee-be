382c4721ad114ac7e6fef2f710f51c83c9128f90
culi <culitif@tuta.io>
2021-07-08 11:41:44 -0700

    write test for POST /auth/signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,28 @@ import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('auth routes', () => {
+  // test data
+  const user = {
+    username: 'me',
+    password: 'password',
+    phoneNumber: '14206661234'
+  };
+
   beforeEach(() => {
     return setup(pool);
+  });
+
+  test('POST user to /auth/signup', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+    ;
+
+    expect(res.body).toEqual({
+      id: '1',
+      username: user.username,
+      phoneNumber: user.phoneNumber,
+      passwordHash: expect.any(String)
+    });
   });
 });
