615c1ed39185d5a00c53e1391fd87aec76b2f0c1
Tis Lais <43742374+tislais@users.noreply.github.com>
2021-07-08 16:28:56 -0700

    Lookout spots (#3)
    * Add controller for findall spots with passing test
    
    * new sql tables
    
    * failing get by id route for spots
    
    * method for get by id spots
    
    * controller for get by id spot
    
    * passing test for get by id spot
    
    * Add failing spot put test
    
    * Add put route for spots with passing test
    
    * changed items to tags
    
    * added jwt
    
    * failing delete test for spots
    
    * route for delete spots
    
    * method for delete spots
    
    * updated cookie parser
    
    * Add auth fix for spots routes with all tests passing
    
    Co-authored-by: Annaleigh Thomas <annaleighxvx@gmail.com>

--- __tests__/spots.test.js
+++ __tests__/spots.test.js
@@ -3,30 +3,53 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Spot from '../lib/models/Spot.js';
+import UserService from '../lib/services/UserService.js';
 
 const home = {
   name: 'home',
-  // userId: '1',
+  userId: '1',
   radius: 1,
   latitude: '45.505100',
   longitude: '-122.675000',
+  tags: ['couch', 'lamp']
 };
 
 const work = {
   name: 'work',
-  // userId: '1',
+  userId: '1',
   radius: 1,
   latitude: '45.505100',
   longitude: '-122.675000',
+  tags: ['couch', 'lamp']
 };
 
+
 describe('spot routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+
+  const agent = request.agent(app);
+  let user;
+
+  beforeEach(async () => {
+    await setup(pool);
+
+    user = await UserService.create({
+      username: 'Tis',
+      password: 'password',
+      phoneNumber: '150412344555'
+    });
+
+    await agent
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'Tis',
+        password: 'password'
+      });
+
+    return user;
   });
 
   it('creates a spot via POST', async () => {
-    const res = await request(app)
+    const res = await agent
       .post('/api/v1/spots')
       .send(home);
 
@@ -42,7 +65,7 @@ describe('spot routes', () => {
     await Spot.create(home);
     await Spot.create(work);
 
-    const res = await request(app)
+    const res = await agent
       .get('/api/v1/spots');
     
     const expected = [
@@ -59,7 +82,49 @@ describe('spot routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('gets a spot by ID using GET', async () => {
+    const spot = await Spot.create(home);
 
+    const res = await agent
+      .get(`/api/v1/spots/${spot.id}`);
+
+    const expected = {
+      id: '1',
+      ...home
+    };
+
+    expect(res.body).toEqual(expected);
+  });
+
+  it('updates a spot via PUT', async () => {
+    const spot = await Spot.create(home);
+    spot.radius = '2';
+    
+    const res = await agent
+      .put(`/api/v1/spots/${spot.id}`)
+      .send(spot);
+
+    const expected = {
+      id: '1',
+      name: 'home',
+      userId: '1',
+      radius: 2,
+      latitude: '45.505100',
+      longitude: '-122.675000',
+      tags: ['couch', 'lamp']
+    };
+    
+    expect(res.body).toEqual(expected);
+  });
+
+  it('deletes a spot via DELETE', async () => {
+    const spot = await Spot.create(home);
+
+    const res = await agent
+      .delete(`/api/v1/spots/${spot.id}`);
+
+    expect(res.body).toEqual(spot);
+  });
 
 
 
