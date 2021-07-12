45b816bfa020b495431e13f615d69a750977231f
culi <culitif@tuta.io>
2021-07-12 14:33:06 -0700

    add reverse geocode test

--- __tests__/geolocation.test.js
+++ __tests__/geolocation.test.js
@@ -1,4 +1,6 @@
 import { isWithinRadius, geocode } from '../lib/utils/geolocation.js';
+import request from 'supertest';
+import app from '../lib/app.js';
 
 describe('radius tests', () => {
 
@@ -15,12 +17,14 @@ describe('radius tests', () => {
   });
 });
 
-describe ('geocoding tests', () => {
+describe ('geocoding routes tests', () => {
   
-  test('receives address and returns lat/long', async () => {
+  test('POST to /geo/geocode/:address receives address and returns lat/long', async () => {
     const input = '1313 Disneyland Dr Anaheim, CA';
 
-    const actual = await geocode(input);
+    const actual = (await request(app)
+      .post(`/api/v1/geo/geocode/${input}`)).body;
+
     const expected = {
       latitude: 33.812371,
       longitude: -117.920431
@@ -29,4 +33,28 @@ describe ('geocoding tests', () => {
     expect(Number(actual.latitude)).toBeCloseTo(expected.latitude, 2);
     expect(Number(actual.longitude)).toBeCloseTo(expected.longitude, 1);
   });
+
+  test('POST to /geo/reverse-geocode', async () => {
+    const input = {
+      latitude: 33.812371,
+      longitude: -117.920431
+    };
+
+    const actual = (await request(app)
+      .post('/api/v1/geo/reverse-geocode')
+      .send(input)
+    ).body;
+
+    const expected = {
+      street: 'Big Thunder Mountain Fastpass Distubtion',
+      postalCode: '217',
+      city: 'Anaheim',
+      state: 'CA',
+      country: 'US'
+    };
+    // 'Big Thunder Mountain Fastpass Distubtion\nAnaheim, CA 217\nUS';
+
+    expect(actual).toEqual(expected);
+  });
+
 });
