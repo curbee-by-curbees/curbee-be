cc92bea2ab054ce4d31afb0058ce46a7385cae71
cmmerritt <christiane.merritt@gmail.com>
2021-07-11 18:18:18 -0700

    playing around with twilio

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 import app from './lib/app.js';
 import pool from './lib/utils/pool.js';
+import * as http from 'http';
+import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+});
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('a message');
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express sever listening on port 1337');
 });
 
 process.on('exit', () => {
