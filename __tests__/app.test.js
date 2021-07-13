4aae945f245237ac8cefc49193d3caf998dd989d
Katherine Tam <katherinemtam@gmail.com>
2021-07-13 11:11:01 -0700

    change number in auth test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,32 +4,32 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
 
-  describe('auth routes', () => {
+describe('auth routes', () => {
   // test data
-    const user = {
-      username: 'me',
-      password: 'password',
-      phoneNumber: '14206661234'
-    };
+  const user = {
+    username: 'me',
+    password: 'password',
+    phoneNumber: '+15036106163‬'
+  };
 
-    beforeEach(() => {
-      return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    test('POST user to /auth/signup', async () => {
-      const res = await request(app)
-        .post('/api/v1/auth/signup')
-        .send(user)
+  test.only('POST user to /auth/signup', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
     ;
 
-      expect(res.body).toEqual({
-        id: '1',
-        username: user.username,
-        phoneNumber: user.phoneNumber,
-        passwordHash: expect.any(String)
-      });
+    expect(res.body).toEqual({
+      id: '1',
+      username: user.username,
+      phoneNumber: user.phoneNumber,
+      passwordHash: expect.any(String)
     });
   });
+  
 
   it('logs a user in via POST', async () => {
     await UserService.create(user);
