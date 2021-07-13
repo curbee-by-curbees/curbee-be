b203301ed0482d1376d0a9e0a0aa1273269e45ff
cmmerritt <christiane.merritt@gmail.com>
2021-07-13 15:02:11 -0700

    add findSubscriberNumbers function and test

--- __tests__/texts.test.js
+++ __tests__/texts.test.js
@@ -2,28 +2,54 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Text from '../lib/models/Text.js';
+import TextService from '../lib/services/TextService.js';
+import Find from '../lib/models/Find.js';
+import User from '../lib/models/User.js';
+import Spot from '../lib/models/Spot.js';
+
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.skip('creates a item in users database with name', async () => {
-    const res = await request(app)
-      .post('/api/v1/texts')
-      .send({ 
-        username: 'Curbee User',
-        passwordHash: 'lalalala',
-        phoneNumber: '+15036106163'
-      });
-    
-    expect(res.body).toEqual({
-      id: 1,
-      username: 'Curbee User',
-      phoneNumber: '+15036106163',
-      passwordHash: 'lalalala'
+  it('returns an array of user phone numbers by subscribed lookout spots', async () => {
+    const cat = await Find.insert({
+      title: 'Porcelain cat statue',
+      isClaimed: false,
+      latitude: '45.519960',
+      longitude: '-122.637980',
+      category: 'decor',
+      tags: ['statue', 'cat']
     });
-  });
+    const user1 = await User.create({
+      username: 'me',
+      password: 'password',
+      phoneNumber: '+15036106163‬'
+    });
+    await Spot.create({
+      name: 'home',
+      userId: '1',
+      radius: 5,
+      latitude: '45.519958',
+      longitude: '-122.637992',
+      tags: ['couch', 'lamp']
+    });
+    const user2 = await User.create({
+      username: 'you',
+      password: 'password',
+      phoneNumber: '+12672100393‬'
+    });
+    await Spot.create({
+      name: 'work',
+      userId: '2',
+      radius: 5,
+      latitude: '45.519965',
+      longitude: '-122.637960',
+      tags: ['couch', 'lamp']
+    });
 
+    const actual = TextService.findSubscriberNumber({ longitude: cat.longitude, latitude: cat.latitude });
+    expect(actual).toEqual([user1.phoneNumber, user2.phoneNumber]);
+  });
 });
