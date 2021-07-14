e998de9f623a01437f84da713a2e3cdc92bea5c8
Katherine Tam <katherinemtam@gmail.com>
2021-07-13 23:28:49 -0700

    adjust finds post test to test TextService when post is created

--- __tests__/texts.test.js
+++ __tests__/texts.test.js
@@ -2,28 +2,21 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import TextService from '../lib/services/TextService.js';
-import Find from '../lib/models/Find.js';
-import UserService from '../lib/services/UserService.js';
-import Spot from '../lib/models/Spot.js';
 import User from '../lib/models/User.js';
+import Find from '../lib/models/Find.js';
+import Spot from '../lib/models/Spot.js';
+import UserService from '../lib/services/UserService.js';
+import FindService from '../lib/services/FindService.js';
+import TextService from '../lib/services/TextService.js';
 
 
-describe('twilio utils', () => {
+describe.skip('twilio utils', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.only('returns an array of user phone numbers by subscribed lookout spots', async () => {
-    const cat = await Find.insert({
-      title: 'Porcelain cat statue',
-      isClaimed: false,
-      latitude: '45.519960',
-      longitude: '-122.637980',
-      category: 'decor',
-      tags: ['statue', 'cat']
-    });
-
+  it('returns an array of user phone numbers by subscribed lookout spots', async () => {
+    
     const user1 = await UserService.create({
       username: 'me',
       password: 'password',
@@ -54,6 +47,15 @@ describe('twilio utils', () => {
       tags: ['couch', 'lamp']
     });    
     
+    const cat = await FindService.create({
+      title: 'Porcelain cat statue',
+      isClaimed: false,
+      latitude: '45.519960',
+      longitude: '-122.637980',
+      category: 'decor',
+      tags: ['statue', 'cat']
+    });
+
     const actual = await TextService.findSubscriberNumbers({ longitude: cat.longitude, latitude: cat.latitude });
     expect(actual).toEqual([user2.phoneNumber]);
   });
