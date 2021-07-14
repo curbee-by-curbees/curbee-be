013ae1fb95faf37c641015ca0c6104c96ed6930d
Katherine Tam <katherinemtam@gmail.com>
2021-07-14 00:30:14 -0700

    adjust dependencies

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import app from './lib/app.js';
 import pool from './lib/utils/pool.js';
-// import http from 'http';
-
 
 const PORT = process.env.PORT || 7890;
 
@@ -9,11 +7,6 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
-
-
-// http.createServer(app).listen(1337, () => {
-//   console.log('Express server listening on port 1337');
-// }); 
 
 process.on('exit', () => {
   console.log('Goodbye!');
